4a42dee8d789348dac4038c0a51edd6c01a0d5bf
snigdhayan <snigdhayan@gmail.com>
2020-06-06 11:27:43 +0200

    Initial commit

---
+++ Ludwig/BreastCancerAnalysisViaLudwig.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+from ludwig.api import LudwigModel
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+# Read data from local directory
+cancer_dataset = pd.read_csv('./breast_cancer_dataset.csv')
+
+# Split data for training and testing
+
+split = 0.3
+cancer_dataset_train, cancer_dataset_test = train_test_split(
+    cancer_dataset, test_size=split)
+# X_train, Y_train = cancer_dataset_train.drop(columns='label'), cancer_dataset_train['label']
+X_test, Y_test = cancer_dataset_test.drop(
+    columns='label'), cancer_dataset_test['label']
+
+# Train locally defined Ludwig model
+
+model = LudwigModel(model_definition_file='./LudwigModelDefinitionFile.yml')
+train_stats = model.train(data_df=cancer_dataset_train,
+                          skip_save_model=True, 
+                          skip_save_processed_input=True, 
+                          skip_save_training_statistics=True, 
+                          skip_save_training_description=True, 
+                          skip_save_log=True, 
+                          skip_save_progress=True)
+
+# Predict and print statistics
+pred = model.predict(data_df=X_test)
+predictions = pred['label_predictions']
+Y_test = Y_test == 1  # Change labels from 0/1 to False/True
+
+pred_correct = []
+for i in range(1, len(Y_test)):
+    pred_correct.append(predictions.iloc[i-1] == Y_test.iloc[i-1])
+
+print("No. of correct predictions = {}".format(sum(pred_correct)))
+print("No. of incorrect predictions = {}".format(len(Y_test)-sum(pred_correct)))
+
+model.close()
---
+++ README.md
@@ -0,0 +1,3 @@
+# NeuralNetworks
+
+This repository contains my experiments with neural networks. Currently there is a TF + Keras based deep neural network that learns to forecast from a handicrafted time series dataset. In the 'Ludwig' directory there is a simple Uber Ludwig based approach to analyze breast cancer.
