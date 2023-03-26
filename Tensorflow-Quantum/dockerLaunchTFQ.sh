d085813ea5b0e1bbbe083d25c20af3b4e30c3bc9
Snigdhayan Mahanta <snigdhayan@gmail.com>
2023-03-26 21:36:24 +0200

    Updated tfq version to 0.7.2

--- Tensorflow-Quantum/dockerLaunchTFQ.sh
+++ Tensorflow-Quantum/dockerLaunchTFQ.sh
@@ -4,6 +4,6 @@ docker build -f ./Dockerfile.txt -t mytfq .
 
 docker run -t -d -p 8888:8888 --name mytfq mytfq
 
-docker exec -it mytfq jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --port=8888
+# docker exec -it mytfq jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --port=8888
 
 # docker system prune -a -f
